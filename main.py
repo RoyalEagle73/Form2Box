56a0a276b680af8a9ce8bd85639f8355632ddab9
Deepak Chauhan <deepakchauhan878@gmail.com>
2019-08-03 08:25:19 +0000

    Fixed low balance message failures, added some more code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-from datetime import datetime
 import configparser as cfg
 
 # Taking out data from config file
@@ -13,10 +12,31 @@ wallet_url = parser.get('creds', 'wallet')										# Reading API URL for your W
 your_number = parser.get('creds', 'your_number')								# Reading emergency contact number
 spreadsheet = parser.get('creds', 'spreadsheet_url')							# Reading Link to your spreadsheet file
 index = parser.get('creds', 'phone_index')										# Reading index of mobile number in main form
+last_size =	int(parser.get('creds', 'last_size'))								# Reading last size of the list 
 
-last_size = 0																	# Initialising last size of the list as 0 
 temp_list = []																	# Taking a temporary list to save garbage data 
 
+## FUNCTION TO UPDATE LAST SIZE IN CONFIG FILE
+def addLastIndex():
+	global last_size
+	temp = ""
+	with open("config.cfg", 'r') as file:
+		temp = file.read()
+		index  = temp.find("last_size = ")
+		temp = temp[:index]
+		temp += "last_size = " + str(last_size)
+	with open("config.cfg", 'w') as file:
+		file.write(temp)
+
+
+
+## FUNCTION TO SEND MESSAGE TO RETRIEVED NUMBER
+def sendSms(message, number):
+	url = "https://www.fast2sms.com/dev/bulk"
+	payload = "sender_id=FSTSMS&message=%s&language=english&route=p&numbers=%s"%(message, number)
+	headers = {'authorization': "%s"%(api_token),'Content-Type': "application/x-www-form-urlencoded",'Cache-Control': "no-cache",}
+	response = requests.request("POST", url, data=payload, headers=headers)
+	return response.json().get('return')
 
 ## FUNCTION TO CHECK BALANCE SO THAT API CAN INFORM OWNER IN CASE OF LOW BALANCE
 def checkBalance():															
@@ -27,15 +47,12 @@ def checkBalance():
     'authorization': "%s"%(api_token),
     }
 	response = requests.request("POST", wallet_url, headers=headers)
-	return response.json().get('wallet')
-
-## FUNCTION TO SEND MESSAGE TO RETRIEVED NUMBER
-def sendSms(message, number):
-	url = "https://www.fast2sms.com/dev/bulk"
-	payload = "sender_id=FSTSMS&message=%s&language=english&route=p&numbers=%s"%(message, number)
-	headers = {'authorization': "%s"%(api_token),'Content-Type': "application/x-www-form-urlencoded",'Cache-Control': "no-cache",}
-	response = requests.request("POST", url, data=payload, headers=headers)
-	return response.json().get('return')
+	if float(response.json().get('wallet')) in [5.0, 4.0, 3.0, 2.0, 1.6, 1.2, 1.0]:
+		k = sendSms("Your wallet balance is low and soon people would be waiting for your response, Kindly recharge . Regards Way2Sms.", your_number)
+		if k == True:
+			print("Low Balance Error generted, SMS sent")
+		else:
+			print("Low Balance Detected but failed to generate SMS")
 
 ## TAKING DATA FROM THE SPREADSHEET 
 def getData():
@@ -68,15 +85,15 @@ def getData():
 
 ## MAIN FUNCTION WORKING HERE
 while True:
-	print(datetime.now())									# TO KEEP AN EYE ON PROGRAM INDICATING IT'S WORKING STATE
+	print("Checking for new data....")									# TO KEEP AN EYE ON PROGRAM INDICATING IT'S WORKING STATE
 	temp_list = getData()
 	if len(temp_list) > last_size:
 		for i in range(0, (len(temp_list)-last_size)):
 			garbage = sendSms(sms, temp_list[last_size+i])
-			if checkBalance() < 25.0:
-				sendSms("Your wallet balance is low and soon people would be waiting for your response, Kindly recharge for continuation of service. Regards Way2Sms.", your_number)
+			checkBalance()
 			if garbage == True:
-				print("SUCCESS *** Sms sent at ", temp_list[last_size+i])
+				print("*** SUCCESS *** Sms sent at ", temp_list[last_size+i])
 			else:
-				print("ERROR !!! Sms failed to send")
+				print("!!! ERROR !!! Sms failed to send")
 		last_size = len(temp_list)
+		addLastIndex()
